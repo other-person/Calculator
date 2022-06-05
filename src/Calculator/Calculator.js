2e43d8d4be7d691f381f406ea650ade330b1fa9c
Tarikclear <bazhenkataras@gmail.com>
2022-06-06 00:00:19 +0400

    Add new functions

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -1,32 +1,60 @@
-import React from "react"
+import React, {useState, ChangeEvent} from "react"
 import s from "./Calculator.module.sass"
 
 export const Calculator = () => {
 
-// пробегаемся циклом по всем кнопкам
+    const [result, setResult] = useState("")
 
+    // click buttons
+    const ClickHandler = (e) => {
+        setResult(result?.concat(e.currentTarget?.name))
+
+    }
+    // clear input
+    const ClearHandler = () => {
+        setResult("")
+    }
+    // backSpace
+    const BackSpaceHandler = () => {
+        setResult(result?.slice(0, -1))
+    }
+    // calculate
+    const CalculateHandler = () => {
+        try {
+            setResult(Number(eval(result).toString()).toFixed(3));
+        }
+        catch(error)
+        {
+            setResult("invalidFormat")
+        }
+    }
     return (
         <div className={s.container}>
             <div className={s.wrapperCalc}>
-                <input className={s.out} type="text" placeholder="000000000000" value="0" disabled/>
+                <input className={s.out} type="text" placeholder="000000000000" value={result}/>
                 <div className={s.buttonsWrapper}>
-                    <div className={s.clearBtn}>CA</div>
-                    <div className={s.nmbBtn}>7</div>
-                    <div className={s.nmbBtn}>8</div>
-                    <div className={s.nmbBtn}>9</div>
-                    <div className={s.operationsBtn} id={s.plusBtn}>+</div>
-                    <div className={s.nmbBtn}>4</div>
-                    <div className={s.nmbBtn}>5</div>
-                    <div className={s.nmbBtn}>6</div>
-                    <div className={s.operationsBtn} id={s.minusBtn}>-</div>
-                    <div className={s.nmbBtn}>1</div>
-                    <div className={s.nmbBtn}>2</div>
-                    <div className={s.nmbBtn}>3</div>
-                    <div className={s.operationsBtn} id={s.divisionBtn}>/</div>
-                    <div className={s.dotBtn}>.</div>
-                    <div className={s.nmbBtn}>0</div>
-                    <div className={s.resultBtn}>=</div>
-                    <div className={s.operationsBtn} id={s.multiplyBtn}>*</div>
+
+                    <div className={s.wrapperCaDel}>
+                        <button className={s.clearBtn} onClick={ClearHandler}>CA</button>
+                        <button className={s.backSpaceBtn} onClick={BackSpaceHandler}>Del</button>
+                    </div>
+
+                    <button className={s.nmbBtn} name="7" onClick={ClickHandler}>7</button>
+                    <button className={s.nmbBtn} name="8" onClick={ClickHandler}>8</button>
+                    <button className={s.nmbBtn} name="9" onClick={ClickHandler}>9</button>
+                    <button className={s.operationsBtn} name="+" onClick={ClickHandler}>+</button>
+                    <button className={s.nmbBtn} name="4" onClick={ClickHandler}>4</button>
+                    <button className={s.nmbBtn} name="5" onClick={ClickHandler}>5</button>
+                    <button className={s.nmbBtn} name="6" onClick={ClickHandler}>6</button>
+                    <button className={s.operationsBtn} name="-" onClick={ClickHandler}>-</button>
+                    <button className={s.nmbBtn} name="1" onClick={ClickHandler}>1</button>
+                    <button className={s.nmbBtn} name="2" onClick={ClickHandler}>2</button>
+                    <button className={s.nmbBtn} name="3" onClick={ClickHandler}>3</button>
+                    <button className={s.operationsBtn} name="/" onClick={ClickHandler}>/</button>
+                    <button className={s.dotBtn} name="." onClick={ClickHandler}>.</button>
+                    <button className={s.nmbBtn} name="0" onClick={ClickHandler}>0</button>
+                    <button className={s.resultBtn} onClick={CalculateHandler}>=</button>
+                    <button className={s.operationsBtn} name="*" onClick={ClickHandler}>*</button>
 
                 </div>
 
